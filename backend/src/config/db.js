8bcc1d356f31e768a74b89882951331a50d00e93
luizpassoss <luiz182004@gmail>
2026-01-02 05:37:57 -0300

    Ajustes1213123111122asssss

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT), // aqui é crucial
   waitForConnections: true,
   connectionLimit: 10
 });
