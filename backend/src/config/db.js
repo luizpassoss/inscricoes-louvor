74fd03eabb22832dc606994ebb227a2e5a6d7fc2
luizpassoss <luiz182004@gmail>
2026-01-02 02:31:35 -0300

    Ajustesqqq

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',        // 👈 seu usuário MySQL
-  password: '',        // 👈 sua senha (vazia se não tiver)
-  database: 'eventos_igreja',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
 module.exports = pool;
